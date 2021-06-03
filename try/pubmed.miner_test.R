83b19ca4db7c67dad73028195492333694ab8a2b
HZYShadow <85103720+HZYShadow@users.noreply.github.com>
2021-06-03 11:09:04 +0800

    Add files via upload

---
+++ try/pubmed.miner_test.R
@@ -0,0 +1,9 @@
+##利用pubmed.mineR进行文本挖掘，获得实体信息
+#abstract-covid19-set.txt为PubMed上直接下载的部分文献摘要，这里作为测试文件
+library(pubmed.mineR)
+pubmed_abstracts<-readabs("abstract-covid19-set.txt") #读入测试文件
+pmid<-pubmed_abstracts@PMID  #获取PMID
+pubtator_output<-pubtator_function(pmid)  #获取PMID对应的注释信息
+
+##这里可以对通过esearch提取出的pubmed_pmid.txt中的PMID进行处理，使用pubtator_function()获得注释信息
+##理论上该方法可行，但是无论怎么调试都出现了报错信息
---
+++ try/pubtatordb_test.R
@@ -0,0 +1,10 @@
+##利用pubtatordb中函数提取实体信息
+#软件包“ pubtatordb”提供了一组函数，这些函数使普通R用户可以轻松地下载“ PubTator”注释，创建然后查询数据库的本地版本。
+install.packages("pubtatordb")
+library(pubtatordb)
+setwd(choose.dir())  #进入文件所在目录
+download_pt(getwd())
+pubtator_path <- file.path(getwd(), "PubTator") 
+pt_to_sql(“PubTator”)    #从pubtator数据创建sqlite数据库
+
+##理论上该方法可以直接建库查询，但在对六类实体的识别和注释存在错误，故该方法也为能够真正实现
